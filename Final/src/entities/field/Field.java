95639eb269301ca17bad24d62954804a02a51bbf
Frederik <frederik@mybid.dk>
2018-01-05 20:42:22 +0100

    Tilføjet en type til Field

--- Final/src/entities/field/Field.java
+++ Final/src/entities/field/Field.java
@@ -1,5 +1,33 @@
 package entities.field;
 
+import entities.enums.FieldType;
+
 public class Field {
+
+	private int fieldNumber;
+	private String text1;
+	private FieldType fieldType;
+
+	public Field(int fieldNo, String text1, FieldType fieldType) {
+		this.fieldNumber = fieldNo;
+		this.text1 = text1;
+		this.fieldType = fieldType;
+	}
+
+	public FieldType getFieldType() {
+		return this.fieldType;
+	}
+
+	public int getFieldNumber() {
+		return this.fieldNumber;
+	}
+
+	public String getText1() {
+		return text1;
+	}
+
+	public String toString() {
+		return String.format("Feltnr: %s\n" + "Text1: %s\\n" + "" + "", getFieldNumber(), getText1());
+	}
 
 }
