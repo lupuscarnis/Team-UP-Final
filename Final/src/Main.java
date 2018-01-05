ec4cf72cddcd0d863cacd9cf644cb0b8936e218e
Frederik <frederik@mybid.dk>
2018-01-05 16:49:42 +0100

    Har refactored lidt i GameCtrl + Main

--- Final/src/Main.java
+++ Final/src/Main.java
@@ -3,9 +3,6 @@ import controllers.GameController;
 import entities.Player;
 
 public class Main {
-
-
-	// Should be moved to Run.java
 	
 	public static void main(String[] args) {
 
@@ -14,70 +11,4 @@ public class Main {
 		gc.play();
 	}
 
-	public class Game {
-
-		// CONSTANTS
-
-		private static final int PLAYER_MIN = 3;
-		private static final int PLAYER_MAX = 6;
-		private static final int DIE_MIN = 1;
-		private static final int DIE_MAX = 6;
-
-		// Maybe move these to bank
-
-		private static final int MONEY_START = 4000; // Amount when passing START
-		private static final int MONEY_JAIL = 1000; // Amount to pay to leave jail
-		private static final int TAX_CASH_AMOUNT = 4000;
-		private static final double TAX_PERCENTAGE_AMOUNT = 0.1;
-
-		// ATTRIBUTES
-		private Player[] players = null;
-
-		/**
-		 * 
-		 * Setup the Game
-		 * 
-		 * @throws Exception
-		 */
-
-		public void setupGame(Player[] players) throws Exception {
-
-			System.out.println("Game: Setup!");
-			
-		}
-
-		/**
-		 * 
-		 * From CDIO3 @ Frederik
-		 * 
-		 * @throws Exception
-		 */
-
-		public void setupGame() throws Exception {
-			setupGame(null);
-		}
-
-		/**
-		 * 
-		 * Main Play() loop
-		 * 
-		 * @throws Exception
-		 */
-
-		public void play() throws Exception {
-			
-			
-			// if is Player inJail?
-			
-			
-            // else Throw Die
-			
-			// MovePlayer
-			
-            // Evaluate Field
-
-		}
-
-
-	}
 }
--- Final/src/controllers/GameController.java
+++ Final/src/controllers/GameController.java
@@ -1,14 +1,74 @@
 package controllers;
 
+import entities.Player;
+
 public class GameController {
 
-	public void play() {
+	// CONSTANTS
 
-		// Setup 
+			private static final int PLAYER_MIN = 3;
+			private static final int PLAYER_MAX = 6;
+			private static final int DIE_MIN = 1;
+			private static final int DIE_MAX = 6;
 
-		// Game loop
-		
-		System.out.println("Så er vi i gang!");
-		
-	}
+			// Maybe move these to bank
+
+			private static final int MONEY_START = 4000; // Amount when passing START
+			private static final int MONEY_JAIL = 1000; // Amount to pay to leave jail
+			private static final int TAX_CASH_AMOUNT = 4000;
+			private static final double TAX_PERCENTAGE_AMOUNT = 0.1;
+
+			// ATTRIBUTES
+			private Player[] players = null;
+
+			/**
+			 * 
+			 * Setup the Game
+			 * 
+			 * @throws Exception
+			 */
+
+			public void setupGame(Player[] players) throws Exception {
+
+				System.out.println("Game: Setup!");
+				
+			}
+
+			/**
+			 * 
+			 * From CDIO3 @ Frederik
+			 * 
+			 * @throws Exception
+			 */
+
+			public void setupGame() throws Exception {
+				setupGame(null);
+			}
+
+			/**
+			 * 
+			 * Main Play() loop
+			 * 
+			 * @throws Exception
+			 */
+
+			public void play(){
+				
+				
+				// if is Player inJail?
+				
+				
+	            // else Throw Die
+				
+				// MovePlayer
+				
+	            // Evaluate Field
+
+			}
+
+	
+	
+	
+	
+	
 }
