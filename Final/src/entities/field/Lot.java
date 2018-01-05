8cacd30d49e4088e72bcc77b9bcbaea5117899f8
Frederik <frederik@mybid.dk>
2018-01-05 16:05:23 +0100

    Tilføjet LotColor

--- Final/src/entities/field/Lot.java
+++ Final/src/entities/field/Lot.java
@@ -1,7 +1,16 @@
 package entities.field;
 
+import entities.enums.LotColor;
+
+/**
+ * Added by Frederik on 05-01-2018 16:04:43 
+ *
+ * Represents lot fields.
+ */
 public class Lot extends Field {
 	
 	int houseCount, hotelCount;
+	
+	LotColor color;
 
 }
