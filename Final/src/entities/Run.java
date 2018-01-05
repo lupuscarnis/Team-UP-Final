1ad18765818bbdd692b1405c8d06692021336b19
Darthlatte <savethecat@gmail.com>
2018-01-05 19:44:58 +0100

    Added call to Game (play)

---
+++ Final/src/entities/Run.java
@@ -0,0 +1,14 @@
+package entities;
+
+import controllers.GameController;
+
+public class Run {
+
+	public static void main(String[] args) throws Exception {
+	
+		Game g = new Game();		
+		g.play();
+		
+	}
+		
+}
