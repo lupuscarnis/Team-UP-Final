fef9dcf4811afd296d924ae4bd2d551702c746c0
Frederik <frederik@mybid.dk>
2018-01-05 09:58:18 +0100

    Så er vi i gang!

---
+++ Final/src/controllers/GameController.java
@@ -0,0 +1,14 @@
+package controllers;
+
+public class GameController {
+
+	public void play() {
+
+		// Setup 
+
+		// Game loop
+		
+		System.out.println("Så er vi i gang!");
+		
+	}
+}
--- Final/src/entities/Main.java
+++ Final/src/entities/Main.java
@@ -1,12 +1,15 @@
 
+package entities;
+
+import controllers.GameController;
+
 public class Main {
-
-	////
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 	
-	// Frederik was here
-
+	
+	public static void main(String[] args) {
+		
+		GameController gc = new GameController();
+		
+		gc.play();
+	}
 }
