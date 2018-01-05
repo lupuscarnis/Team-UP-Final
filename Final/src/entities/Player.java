b8428dd85e15ccb9bd9a75be11232820537383d4
Frederik <frederik@mybid.dk>
2018-01-05 10:21:41 +0100

    Player skal bruge en Net Worth metode

--- Final/src/entities/Player.java
+++ Final/src/entities/Player.java
@@ -2,4 +2,9 @@ package entities;
 
 public class Player {
 
+	
+	//Opgøre sin formue så vi kan løse denne: "Deres kontante penge + skøder + bygninger ikke overstiger kr. 15000."
+	// og måske også Win condition?
+	public int getNetWorth() {return 0;}
+	
 }
