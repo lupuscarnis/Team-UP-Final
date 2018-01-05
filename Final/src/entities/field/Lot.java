edcd3cf60ff0d1b9139dbf5b74e60353813d30f8
Frederik <frederik@mybid.dk>
2018-01-05 16:02:35 +0100

    Tilføjet Field/Lot

---
+++ Final/src/entities/field/Lot.java
@@ -0,0 +1,7 @@
+package entities.field;
+
+public class Lot extends Field {
+	
+	int houseCount, hotelCount;
+
+}
--- Final/src/utilities/MyRandom.java
+++ Final/src/utilities/MyRandom.java
@@ -14,7 +14,6 @@ public class MyRandom {
 	 *            Highest possible value
 	 * @return Value within the boundaries of min and max
 	 */
-
 	public static int randInt(int min, int max) {
 
 		int randomNum = _rand.nextInt((max - min) + 1) + min;
