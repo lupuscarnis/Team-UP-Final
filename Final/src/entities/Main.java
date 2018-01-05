69053c3b702ba6abf2010a7711a5540d2787857d
Darthlatte <savethecat@gmail.com>
2018-01-05 13:58:43 +0100

    Segmenting the Play Loop

--- Final/src/entities/Main.java
+++ Final/src/entities/Main.java
@@ -4,33 +4,55 @@ package entities;
 import controllers.GameController;
 
 public class Main {
-	
-	
+
+
 	public static void main(String[] args) {
-		
+
 		GameController gc = new GameController();
-		
+
 		gc.play();
 	}
-	
+
 	public class Game {
-		
+
 		// CONSTANTS
-		
+
 		private static final int PLAYER_MIN = 3;
 		private static final int PLAYER_MAX = 6;
 		private static final int DIE_MIN = 1;
 		private static final int DIE_MAX = 6;
-		
+
 		// Maybe move these to bank
-		
+
 		private static final int MONEY_START = 4000; // Amount when passing START
 		private static final int MONEY_JAIL = 1000; // Amount to pay to leave jail
 		private static final int TAX_CASH_AMOUNT = 4000;
 		private static final double TAX_PERCENTAGE_AMOUNT = 0.1;
-		
+
 		// ATTRIBUTES
 		private Player[] players = null;
+
+		/**
+		 * 
+		 * Setup the Game
+		 * 
+		 * @throws Exception
+		 */
+
+		public void setupGame(Player[] players) throws Exception {
+
+		}
+
+		/**
+		 * 
+		 * From CDIO3 @ Frederik
+		 * 
+		 * @throws Exception
+		 */
+
+		public void setupGame() throws Exception {
+			setupGame(null);
+		}
 
 		/**
 		 * 
@@ -38,13 +60,20 @@ public class Main {
 		 * 
 		 * @throws Exception
 		 */
-		
+
 		public void play() throws Exception {
+
 			
+			// if is Player inJail?
 			
+            // else Throw Die
 			
+			// MovePlayer
+			
+            // Evaluate Field
+
 		}
-		
-		
+
+
 	}
 }
