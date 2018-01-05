7109cace6056d6aa1c5e22c66ecc4a2af634b644
Darthlatte <savethecat@gmail.com>
2018-01-05 13:44:36 +0100

    Added simple constants to Main and main play loop

--- Final/src/entities/Main.java
+++ Final/src/entities/Main.java
@@ -12,4 +12,39 @@ public class Main {
 		
 		gc.play();
 	}
+	
+	public class Game {
+		
+		// CONSTANTS
+		
+		private static final int PLAYER_MIN = 3;
+		private static final int PLAYER_MAX = 6;
+		private static final int DIE_MIN = 1;
+		private static final int DIE_MAX = 6;
+		
+		// Maybe move these to bank
+		
+		private static final int MONEY_START = 4000; // Amount when passing START
+		private static final int MONEY_JAIL = 1000; // Amount to pay to leave jail
+		private static final int TAX_CASH_AMOUNT = 4000;
+		private static final double TAX_PERCENTAGE_AMOUNT = 0.1;
+		
+		// ATTRIBUTES
+		private Player[] players = null;
+
+		/**
+		 * 
+		 * Main Play() loop
+		 * 
+		 * @throws Exception
+		 */
+		
+		public void play() throws Exception {
+			
+			
+			
+		}
+		
+		
+	}
 }
