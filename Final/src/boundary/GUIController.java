38dbd0608f1ed6409c94324ae475330c05cf002b
hjaltebaekgaard <32597837+hjaltebaekgaard@users.noreply.github.com>
2018-01-06 14:46:06 +0100

    Now initializing cars.

--- Final/src/boundary/GUIController.java
+++ Final/src/boundary/GUIController.java
@@ -60,6 +60,9 @@ public class GUIController {
 		// init. array of GUI_Players
 		gPlayers = new GUI_Player[players.length];
 
+		// init. array of GUI_Cars
+		initializeCarList();
+		
 		// add players to GUI_Players array
 		int index = 0;
 		for (Player p : players) {
