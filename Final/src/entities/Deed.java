1fbc24ab0284f8208ddf4dbfe4fc4f3123911812
163312 <32563877+163312@users.noreply.github.com>
2018-01-06 13:25:53 +0100

    basic deed, hvordan smider vi informationerne ind?

--- Final/src/entities/Deed.java
+++ Final/src/entities/Deed.java
@@ -2,4 +2,34 @@ package entities;
 
 public class Deed {
 
+	
+	public int price;
+	public int housePrice;
+	public int hotelPrice;
+	public int basicRent;
+	public int oneHouseRent;
+	public int twoHouseRent;
+	public int threeHouseRent;
+	public int fourHouseRent;
+	public int hotelRent;
+	public int fieldNumber;
+	public String Field;
+	public String owner = "Bank";
+	public String color;
+	
+	
+	public Deed (int fieldNumber)
+	{
+		this.fieldNumber = fieldNumber;
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
 }
