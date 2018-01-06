b9459fdd65381c50462b25e25864f355400a6164
163312 <32563877+163312@users.noreply.github.com>
2018-01-06 19:55:11 +0100

    all messages

--- Final/src/controllers/FieldLogicController.java
+++ Final/src/controllers/FieldLogicController.java
@@ -26,38 +26,46 @@ public class FieldLogicController {
 		{
 		case BREWERY:
 			BreweryField bf = (BreweryField)field;
-			
+			gui.showMessage("you have landed on a  "+field.getFieldType()+"do you wish to purchase it?");
 			break;
 		case CHANCE:
 			ccc.drawChanceCard();
 			ccc.handleDraw(currentPlayer);
-			gui.showMessage();
+			gui.showMessage("you have landed on "+field.getFieldType()+"");
 			// ingen grund til cast da den bare er en Field type
 			break;
 		case EXTRATAX:
+			gui.showMessage("you have landed on "+field.getFieldType());
 			// ingen grund til cast da den bare er en Field type
 			break;
 		case FREEPARKING:
+			gui.showMessage("you have landed on "+field.getFieldType());
 			// ingen grund til cast da den bare er en Field type
 			break;
 		case GOTOJAIL:
+			gui.showMessage("you have landed on "+field.getFieldType());
 			// ingen grund til cast da den bare er en Field type
 			break;
 		case INCOMETAX:
+			gui.showMessage("you have landed on "+field.getFieldType());
 			// ingen grund til cast da den bare er en Field type
 			break;
 		case LOT:
 			LotField lf = (LotField)field;
 			
+			gui.showMessage("you have landed on "+field.getFieldType());
 			break;
 		case SHIPPING:
 			ShippingField sf = (ShippingField)field;
 			
+			gui.showMessage("you have landed on "+field.getFieldType());
 			break;
 		case START:
+			gui.showMessage("you have landed on "+field.getFieldType());
 			// ingen grund til cast da den bare er en Field type
 			break;
 		case VISITJAIL:
+			gui.showMessage("you have landed on "+field.getFieldType());
 			// ingen grund til cast da den bare er en Field type
 			break;
 		default:			
