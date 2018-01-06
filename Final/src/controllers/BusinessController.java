77d9b4692fa23a446091621cd3f23eaf542aaeb6
Frederik <frederik@mybid.dk>
2018-01-06 20:39:29 +0100

    Added new controller

---
+++ Final/src/controllers/BusinessController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+/**
+ * Added by Frederik on 06-01-2018 20:38:39 
+ * 
+ * Class intended to hold logic reg. business transactions etc.
+ *
+ */
+public class BusinessController {
+
+	public int calculateRent()
+	{
+		return 0;
+	}
+}
