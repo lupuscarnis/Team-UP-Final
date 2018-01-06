51202ac986bfd65c28c31a52a2976c84616d29cb
Frederik <frederik@mybid.dk>
2018-01-06 20:49:46 +0100

    Added methods to Bus. logic

---
+++ Final/src/controllers/BusinessLogicController.java
@@ -0,0 +1,35 @@
+package controllers;
+
+import entities.Player;
+import entities.field.OwnableField;
+
+/**
+ * Added by Frederik on 06-01-2018 20:38:39
+ * 
+ * Class intended to hold logic reg. business transactions etc.
+ *
+ */
+// TODO: Implement class
+public class BusinessLogicController {
+
+	// Calculate rent for field
+	public int calculateRent(OwnableField field) {
+		// TODO: Implement method
+		return 0;
+	}
+
+	// Pawn lot
+	public void pawnLot(OwnableField field) {
+		// TODO: Implement method
+	}
+
+	// set owner of (ownable)field.
+	public void setOwner(OwnableField field, Player owner) {
+		// TODO: Implement
+	}
+
+	// set owner of Get Out Of Jail Card.
+	public void setGetOutOfJailCard(Player owner) {
+		// TODO: Implement
+	}
+}
