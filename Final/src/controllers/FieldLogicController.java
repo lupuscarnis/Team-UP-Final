51d7e9775894ce4bbf2f02f98954a34f5060e8ad
163312 <32563877+163312@users.noreply.github.com>
2018-01-07 11:55:16 +0100

    fjernet ikke brugte ting

--- Final/src/controllers/FieldLogicController.java
+++ Final/src/controllers/FieldLogicController.java
@@ -4,9 +4,11 @@ import entities.Player;
 import entities.field.BreweryField;
 import entities.field.Field;
 import entities.field.LotField;
+import entities.field.OwnableField;
 import entities.field.ShippingField;
 import gui_main.GUI;
 import utilities.FieldLoader;
+import boundary.GUIController;
 
 public class FieldLogicController {
 	FieldLoader Fl = new FieldLoader();
@@ -15,29 +17,41 @@ public class FieldLogicController {
 	private boundary.GUIController guic;
 	private GUI gui = null;
 	Player currentPlayer;
-	Field[] fieldlist = new Field[40];
-	Field[] Fieldlist = Field[40];
+	
+//	Field[] Fieldlist = Field[40];
 	
 	
-	for (int i = 0; i < fieldlist.length; i++) 
-	{
-		Field tmp = gbc.getFieldByNumber(i + 1);
-
-		String title = tmp.getTitle();
-		String text = tmp.getDesc();
-}
 
 
-	// kan ikke finde en "currentplayer" i Game endnu, taenker at jeg kommer til at bruge den
+
+	// kan ikke finde en "currentplayer" i Game endnu, taenker at jeg kommer til at bruge den. 
 	//Game.getCurrentPlayer()
 	
 	public FieldLogicController(GameBoardController gbc) {
 		this.gbc=gbc;
 	}
 	
+//	public int getPrice(int fieldnumber){
+//	int price = 0;
+//	OwnableField theField = OwnableField(null, fieldnumber, null) ;
+//		return price;
+//		det her gik ikke, har brug for metode der giver mig prisen, med indputtet fieldnumber. kig p[ getfieldbynumber fra bpundary
+//	}
+	
 	
 	public void resolveField(Field field) throws Exception
 	{
+		{
+		
+//		Field[] fieldlist = new Field[40];
+//		for (int i = 0; i < fieldlist.length; i++) 
+//		{
+//			Field tmp = gbc.getFieldByNumber(i + 1);
+//
+//			String title = tmp.getTitle();
+//			String text = tmp.getDesc();
+		}
+		
 		switch(field.getFieldType())
 		{
 		case BREWERY:
@@ -52,6 +66,7 @@ public class FieldLogicController {
 			break;
 		case EXTRATAX:
 			gui.showMessage("you have landed on "+field.getFieldType());
+			currentPlayer.withdraw(2000);
 			// ingen grund til cast da den bare er en Field type
 			break;
 		case FREEPARKING:
@@ -60,28 +75,33 @@ public class FieldLogicController {
 			break;
 		case GOTOJAIL:
 			gui.showMessage("you have landed on "+field.getFieldType());
+			guic.movePlayer(currentPlayer, 10);
+			// er saa vidt jeg forstaar ikke muligt at implementere pt.
 			// ingen grund til cast da den bare er en Field type
 			break;
 		case INCOMETAX:
 			gui.showMessage("you have landed on "+field.getFieldType());
+			currentPlayer.withdraw(4000);
+			// at implementere valget der bruger 10% maa vente lidt
 			// ingen grund til cast da den bare er en Field type
 			break;
 		case LOT:
 			LotField lf = (LotField)field;
-			
-			gui.showMessage("you have landed on "+field.getFieldType());
+			//mangler metode
+		//currentPlayer.getCurrentField().getPrice();
+			gui.showMessage("you have landed on "+field.getFieldType()+" do you wish to purchase it?");
 			break;
 		case SHIPPING:
 			ShippingField sf = (ShippingField)field;
-			
-			gui.showMessage("you have landed on "+field.getFieldType());
+			gui.showMessage("you have landed on "+field.getFieldType()+" do you wish to purchase it?");
 			break;
 		case START:
-			gui.showMessage("you have landed on "+field.getFieldType());
+			gui.showMessage("you have landed on "+field.getFieldType()+" you gain 4000 kr.");
+			currentPlayer.deposit(4000);
 			// ingen grund til cast da den bare er en Field type
 			break;
 		case VISITJAIL:
-			gui.showMessage("you have landed on "+field.getFieldType());
+			gui.showMessage("you have landed on "+field.getFieldType() +" you are here on a visit, nothing happens.");
 			// ingen grund til cast da den bare er en Field type
 			break;
 		default:			
