8b9078a83177193934a674f94051038326376c60
hjaltebaekgaard <32597837+hjaltebaekgaard@users.noreply.github.com>
2018-01-09 01:41:54 +0100

    Added options (removed wild EndTurn)

--- Final/src/entities/enums/UserOption.java
+++ Final/src/entities/enums/UserOption.java
@@ -5,8 +5,14 @@ public enum UserOption {
 	//StartTurn,
 	ThrowDice, 
 	//StartGame, 
-	PawnLot, 
+	PawnLot,
+	UnPawnLot,
+	HandleBuildings,
 	BuyHotel, 
+	//this is a special case, need to either allow downgrade from hotel to houses (would be bending game rules) or remove ALL hotels of that lot category
+	SellHotel,
 	BuyHouse, 
+	SellHouse,
+	//InvalidOption!! :)
 	EndTurn
 }
