bcb05eed7471c357d3ff0ac301fb79a3bb8092ae
Frederik <frederik@mybid.dk>
2018-01-14 12:10:25 +0100

    Cleaned ReceiveChanceCard

--- Final/src/entities/chancecard/ReceiveChanceCard.java
+++ Final/src/entities/chancecard/ReceiveChanceCard.java
@@ -1,15 +1,25 @@
 package entities.chancecard;
 
+/**
+ * Chance card sub class that represents cards where player gets to receive
+ * money.
+ *
+ */
 public class ReceiveChanceCard extends ChanceCard {
 
 	private int amount;
 
 	public ReceiveChanceCard(int id, String text, int receivableAmount) {
 		super(id, text);
-		
+
 		this.amount = receivableAmount;
 	}
 
+	/**
+	 * Gets amount player will receive.
+	 * 
+	 * @return
+	 */
 	public int getAmount() {
 		return amount;
 	}
@@ -17,9 +27,8 @@ public class ReceiveChanceCard extends ChanceCard {
 	public void setAmount(int amount) {
 		this.amount = amount;
 	}
-	
-	public String toString()
-	{
-		return super.toString() + String.format("\nAmount: %s\n", getAmount());	
+
+	public String toString() {
+		return super.toString() + String.format("\nAmount: %s\n", getAmount());
 	}
 }
