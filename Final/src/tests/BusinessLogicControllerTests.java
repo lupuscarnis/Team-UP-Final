bba0bf62c980f75986eceffc8eb14b849478b0f3
163312 <32563877+163312@users.noreply.github.com>
2018-01-14 20:11:17 +0100

    buyLot "tester" ogsaa om der traekkes penge

--- Final/src/tests/BusinessLogicControllerTests.java
+++ Final/src/tests/BusinessLogicControllerTests.java
@@ -80,6 +80,8 @@ public class BusinessLogicControllerTests {
 
 		for (OwnableField field : gbc.getAllOwnableFields()) {
 			
+			p.setBalance(10000);
+			
 			// field not owned
 			assertTrue(field.getOwner() == null);
 			
@@ -90,6 +92,8 @@ public class BusinessLogicControllerTests {
 
 			// field owned
 			assertTrue(field.getOwner() == p);
+			
+			assertTrue(10000-field.getPrice()== p.getBalance());
 		}
 	}
 
