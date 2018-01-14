5c3c0abc98a52769c3e5b0e2301ba13effacefa5
Frederik <frederik@mybid.dk>
2018-01-14 17:29:58 +0100

    testSetOwner() done

--- Final/src/tests/BusinessLogicControllerTests.java
+++ Final/src/tests/BusinessLogicControllerTests.java
@@ -10,6 +10,8 @@ import org.junit.Test;
 
 import controllers.BusinessLogicController;
 import controllers.GameBoardController;
+import entities.Player;
+import entities.field.OwnableField;
 
 public class BusinessLogicControllerTests {
 
@@ -21,13 +23,13 @@ public class BusinessLogicControllerTests {
 	public static void tearDownAfterClass() throws Exception {
 	}
 
-
 	private GameBoardController gbc;
+	private BusinessLogicController blc;
 
 	@Before
 	public void setUp() throws Exception {
 		gbc = GameBoardController.getInstance();
-		blc= new BusinessLogicController();
+		blc = new BusinessLogicController();
 	}
 
 	@After
@@ -35,154 +37,131 @@ public class BusinessLogicControllerTests {
 		gbc.destroy();
 	}
 
-	
-	public void testBusinessLogicController() {
-		 
-	}
-
-	
 	public void testPawnLotOwnableField() {
-		 
+
 	}
 
-	
+	@Test
 	public void testSetOwner() {
-		 
+		
+		Player p = new Player("Gurli", 520);
+
+		for (OwnableField field : gbc.getAllOwnableFields()) {
+
+			// assert not owned
+			assertTrue(field.getOwner() == null);
+
+			// change owner state
+			blc.setOwner(field, p);
+		}
 	}
 
-	
 	public void testSetGetOutOfJailCard() {
-		 
+
 	}
 
-	
 	public void testBuyLot() {
-		 
+
 	}
 
-	
 	public void testPayRent() {
-		 
+
 	}
 
-	
 	public void testPlayerNetWorth() {
-		 
+
 	}
 
-	
 	public void testBuildHouse() {
-		 
+
 	}
 
-	
 	public void testBuildHotel() {
-		 
+
 	}
 
-	
 	public void testEvaluatePlayer() {
-		 
+
 	}
 
-	
 	public void testPayIncomeTax() {
-		 
+
 	}
 
-	
 	public void testUserCanAffordLot() {
-		 
+
 	}
 
-	
 	public void testUserCanAffordHouse() {
-		 
+
 	}
 
-	
 	public void testUserCanAffordHotel() {
-		 
+
 	}
 
-	
 	public void testPlayerCanBuildHouse() {
-		 
+
 	}
 
-	
 	public void testPlayerCanBuildHotel() {
-		 
+
 	}
 
-	
 	public void testHasHouse() {
-		 
+
 	}
 
-	
 	public void testHasHotel() {
-		 
+
 	}
 
-	
 	public void testSellHouse() {
-		 
+
 	}
 
-	
 	public void testSellHotel() {
-		 
+
 	}
 
-	
 	public void testGetFieldsWithHouses() {
-		 
+
 	}
 
-	
 	public void testGetFieldsWithHotels() {
-		 
+
 	}
 
-	
 	public void testGetPawnableFields() {
-		 
+
 	}
 
-	
 	public void testGetPawnedFields() {
-		 
+
 	}
 
-	
 	public void testHasPawn() {
-		 
+
 	}
 
-	
 	public void testCanPawn() {
-		 
+
 	}
 
-	
 	public void testPawnLotStringPlayer() {
-		 
+
 	}
 
-	
 	public void testAuction() {
-		 
+
 	}
 
-	
 	public void testUnpawn() {
-		 
-	}
 
+	}
 
 	public void testDestroyPlayer() {
-		 
+
 	}
 
 }
